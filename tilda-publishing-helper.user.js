003fbd08abe9c5eee68f3e7a17c662a809e7a595
roman-kosov <i-am-roman-kosov@ya.ru>
2019-03-15 00:20:01 +0300

    new version

--- tilda-publishing-helper.user.js
+++ tilda-publishing-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tilda Publishing Helper
 // @namespace    https://roman-kosov.ru
-// @version      32.6
+// @version      32.7
 // @description  try to take over the world!
 // @author       Roman Kosov
 // @copyright    2017 - 2019, Roman Kosov (https://greasyfork.org/users/167647)
