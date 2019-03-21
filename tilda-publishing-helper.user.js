0096bb67a5937e2c0073700e949a5f9ddf8474a1
roman-kosov <i-am-roman-kosov@ya.ru>
2019-03-21 12:36:05 +0300

    oopsie

--- tilda-publishing-helper.user.js
+++ tilda-publishing-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tilda Publishing Helper
 // @namespace    https://roman-kosov.ru
-// @version      33.2
+// @version      33.2.1
 // @description  try to take over the world!
 // @author       Roman Kosov
 // @copyright    2017 - 2019, Roman Kosov (https://greasyfork.org/users/167647)
@@ -13,7 +13,7 @@
 // @exclude      https://experts.tilda.cc/*
 // @exclude      https://members.tilda.cc/*
 // @exclude      https://crm.tilda.cc/*
-// @exclude      https://news.tilda.cc/*
+// @exclude      https://news.tildacdn.com/*
 // @license      MIT
 // ==/UserScript==
 (async function (window, undefined) {
