614f2cfa0406aabad11c1317ce207b67f76ff5ea
roman-kosov <i-am-roman-kosov@ya.ru>
2019-05-09 00:01:20 +0300

    Update tilda-publishing-helper.user.js

--- tilda-publishing-helper.user.js
+++ tilda-publishing-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tilda Publishing Helper
 // @namespace    https://roman-kosov.ru
-// @version      36.3
+// @version      36.4
 // @description  try to take over the world!
 // @author       Roman Kosov
 // @copyright    2017 - 2019, Roman Kosov (https://greasyfork.org/users/167647)
@@ -776,14 +776,6 @@
                                 </table>
                             </a>
                         `).appendTo($(".td-project-uppanel__wrapper").find("a[href^='/projects/leads/?projectid=']"));
-                    }
-
-                    if ($("img[src$='td-icon-catalog.png']").length > 0 && $("img[src$='td-icon-del-black.png']").length > 0 && $(".td-project-uppanel__url").width() > 250) {
-                        styleBody += `
-                            .td-project-uppanel__button {
-                                margin-right: 34px !important;
-                            }
-                        `;
                     }
 
                     $('.td-page').each(function name(params) {
