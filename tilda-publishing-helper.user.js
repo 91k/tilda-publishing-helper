cc61599459be5ce1975357a2c6e243a46a0c576e
roman-kosov <i-am-roman-kosov@ya.ru>
2019-08-13 17:41:02 +0300

    Новая версия
    41. Указываем количество ошибок в заявках
    40. Сообщение «Пробный тариф закончится через ... д.»

--- tilda-publishing-helper.user.js
+++ tilda-publishing-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tilda Publishing Helper
 // @namespace    https://roman-kosov.ru
-// @version      39.1
+// @version      41.0
 // @description  try to take over the world!
 // @author       Roman Kosov
 // @copyright    2017 - 2019, Roman Kosov (https://greasyfork.org/users/167647)
