02ad303ae9a1524eed7c6bae213a5a65f4e4251b
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-03-06 16:51:43 +0300

    Using script like a library

--- tilda-show-blocks-safari.user.js
+++ tilda-show-blocks-safari.user.js
@@ -1,28 +1,17 @@
 // ==UserScript==
 // @name         Показать блоки Tilda на странице (Safari ver.)
 // @namespace    https://roman-kosov.ru/donate
-// @version      1.0.6
+// @version      2.0.1
 // @description  Показать какие блоки используются на странице сделанных в Тильде (автоматическая версия, Safari ver.)
 // @author       Roman Kosov
 // @copyright    2020 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
 // @include      *
 // @exclude      https://tilda.cc/*
 // @run-at       document-end
+// @require      https://greasyfork.org/scripts/441018.js
 // @icon         https://www.google.com/s2/favicons?domain=https://tilda.cc
 // ==/UserScript==
 
 (function () {
     'use strict';
-    const isTilda = document.querySelector('div#allrecords');
-    const isTildaEmail = document.querySelector("table#allrecords[data-tilda-email='yes']");
-    const isTildaCC = document.location.host.includes('tilda.cc');
-
-    if (!document.querySelector('#tilda-helper-script') && (isTilda || isTildaEmail) && !isTildaCC) {
-        const script = document.createElement('script');
-        script.id = 'tilda-helper-script';
-        script.src = 'https://cdn.jsdelivr.net/gh/roman-kosov/tilda-publishing-helper/tilda-show-blocks-helper.min.js';
-        if (document.body) {
-            document.body.appendChild(script);
-        }
-    }
 })();
