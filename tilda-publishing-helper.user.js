ff0c969519e502f290558f3856507ef85f15f23e
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-03-11 21:28:35 +0300

    Update tilda-publishing-helper.user.js

--- tilda-publishing-helper.user.js
+++ tilda-publishing-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tilda Publishing Helper
 // @namespace    https://roman-kosov.ru/donate
-// @version      55.0.2
+// @version      55.0.3
 // @description  Тильда Хелпер: вспомогательные фичи, апгрейд Zero блока
 // @author       Roman Kosov
 // @copyright    2017 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
