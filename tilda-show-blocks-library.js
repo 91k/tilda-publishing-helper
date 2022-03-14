b26ae600d80ef103dd6825505740248a6b77d2ac
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-03-14 15:46:31 +0300

    Update tilda-show-blocks-library.js

--- tilda-show-blocks-library.js
+++ tilda-show-blocks-library.js
@@ -1,15 +1,10 @@
 // ==UserScript==
 // @name         Выводит информацию о используемых блоках, если сайт сделан на Тильде
 // @namespace    https://roman-kosov.ru/donate
-// @version      1.0.2
+// @version      1.0.3
 // @description  Показать какие блоки используются на странице сделанных в Тильде
 // @author       Roman Kosov
-// @copyright    2017 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
-// @include      *
-// @exclude      tilda.cc
-// @exclude      help-ru.tilda.cc
-// @exclude      help.tilda.cc
-// @exclude      tilda.education
+// @copyright    2018 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
 // @icon         https://www.google.com/s2/favicons?domain=https://tilda.cc
 // ==/UserScript==
 
