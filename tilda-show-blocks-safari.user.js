d2dec336ee90f7baee90886e9ea6f11f886f6fb5
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-03-14 15:46:29 +0300

    Update tilda-show-blocks-safari.user.js

--- tilda-show-blocks-safari.user.js
+++ tilda-show-blocks-safari.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Показать блоки Tilda на странице (Safari ver.)
 // @namespace    https://roman-kosov.ru/donate
-// @version      2.0.1
+// @version      2.0.2
 // @description  Показать какие блоки используются на странице сделанных в Тильде (автоматическая версия, Safari ver.)
 // @author       Roman Kosov
 // @copyright    2020 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
 // @include      *
-// @exclude      https://tilda.cc/*
+// @exclude      tilda.cc
+// @exclude      help.tilda.cc
+// @exclude      help-ru.tilda.cc
+// @exclude      tilda.education
 // @run-at       document-end
-// @require      https://greasyfork.org/scripts/441018.js
+// @require      https://greasyfork.org/scripts/441018.js?version=1028179
 // @icon         https://www.google.com/s2/favicons?domain=https://tilda.cc
 // ==/UserScript==
 
