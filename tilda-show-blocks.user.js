64e349c3a3d70f705829b75b10281a96ba69cde8
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-03-14 15:46:28 +0300

    Update tilda-show-blocks.user.js

--- tilda-show-blocks.user.js
+++ tilda-show-blocks.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         Показать блоки Tilda на странице
 // @namespace    https://roman-kosov.ru/donate
-// @version      2.0.1
+// @version      2.0.2
 // @description  Показать какие блоки используются на странице сделанных в Тильде
 // @author       Roman Kosov
 // @copyright    2018 - 2022, Roman Kosov (https://greasyfork.org/users/167647)
 // @include      *
-// @exclude      https://tilda.cc/*
+// @exclude      tilda.cc
+// @exclude      help.tilda.cc
+// @exclude      help-ru.tilda.cc
+// @exclude      tilda.education
 // @run-at       context-menu
-// @require      https://greasyfork.org/scripts/441018.js
+// @require      https://greasyfork.org/scripts/441018.js?version=1028179
 // @icon         https://www.google.com/s2/favicons?domain=https://tilda.cc
 // ==/UserScript==
 
