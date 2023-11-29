fd918973506618e3923c90570cd4db6263554239
Roman Kosov <11503318+91k@users.noreply.github.com>
2023-11-29 21:59:14 +0300

    fix: корректная регулярка - учитывается урл с параметрами

--- tilda-show-blocks-safari.user.js
+++ tilda-show-blocks-safari.user.js
@@ -2,11 +2,11 @@
 // @name         Показать блоки Tilda на странице (Safari ver.)
 // @namespace    https://roman-kosov.ru/donate
 // @homepage     https://roman-kosov.ru
-// @version      2.0.6
+// @version      2.0.7
 // @description  Показать какие блоки используются на странице сделанных в Тильде (активируется автоматически)
 // @author       Roman Kosov
 // @copyright    2020 - 2077, Roman Kosov (https://greasyfork.org/users/167647)
-// @match        https://*
+// @match        *://*/*
 // @exclude      https://tilda.ru/*
 // @exclude      https://tilda.cc/*
 // @exclude      https://tilda.by/*
--- tilda-show-blocks.user.js
+++ tilda-show-blocks.user.js
@@ -2,11 +2,11 @@
 // @name         Показать блоки Tilda на странице
 // @namespace    https://roman-kosov.ru/donate
 // @homepage     https://roman-kosov.ru
-// @version      2.0.6
+// @version      2.0.7
 // @description  Показать какие блоки используются на странице сделанных в Тильде
 // @author       Roman Kosov
 // @copyright    2018 - 2077, Roman Kosov (https://greasyfork.org/users/167647)
-// @match        https://*
+// @match        *://*/*
 // @exclude      https://tilda.ru/*
 // @exclude      https://tilda.cc/*
 // @exclude      https://tilda.by/*
