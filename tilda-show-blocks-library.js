fe7dbb580b72709d954aaf0dcb576d2d1fd85006
Roman Kosov <11503318+91k@users.noreply.github.com>
2023-11-29 21:50:55 +0300

    fix: нашёл корректную регулярку чтобы учитывались урлы с параметрами

--- tilda-show-blocks-library.js
+++ tilda-show-blocks-library.js
@@ -6,7 +6,7 @@
 // @description  Показать какие блоки используются на странице сделанных в Тильде
 // @author       Roman Kosov
 // @copyright    2018 - 2077, Roman Kosov (https://greasyfork.org/users/167647)
-// @match        https://*
+// @match        *://*/*
 // @exclude      https://tilda.ru/*
 // @exclude      https://tilda.cc/*
 // @exclude      https://tilda.by/*
